8259068301bdb371df52ed95a85c28502506414c
ovie009 <ovieiffieu2much@gmail.com>
2022-06-01 06:54:15 +0100

    outputting list with map and giving them unique keys

--- src/Home.js
+++ src/Home.js
@@ -1,22 +1,21 @@
 import { useState } from "react";
 
 const Home = () => {
-    // let name = 'Ovie'; // unreactive variable
 
-    const [name, setName] = useState('Spencer'); // reactive variable
-    const [age, setAge] = useState(24); // reactive variable
-
-    const handleClick = () => {
-        // setName, function to change state of variable
-        setName('Mike');
-        setAge(27);
-    }
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ])
 
     return ( 
         <div className="home">
-            <h2>Homepage</h2> <br /><br />
-            <p>{name} is {age} years old</p> <br /><br />
-            <button onClick={handleClick}>Click Me</button> <br />
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                </div>
+            ))}
         </div>
     );
 }
