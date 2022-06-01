170c4489e203383585f4a9947f22b811fe01edc2
ovie009 <ovieiffieu2much@gmail.com>
2022-06-01 05:34:03 +0100

    using e.target to get target element details

--- src/Home.js
+++ src/Home.js
@@ -5,7 +5,7 @@ const Home = () => {
     }
 
     const handleClickAgain = (name, e) => {
-        console.log('Hello, '+name);
+        console.log('Hello, '+name+'Target Event: '+e.target);
     }
 
     return ( 
