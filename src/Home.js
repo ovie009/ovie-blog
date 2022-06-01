4ecbd2f55a5f6fd38eb4af30c7187d71ffd0518b
ovie009 <ovieiffieu2much@gmail.com>
2022-06-01 05:30:27 +0100

    using running functions with on click event and accessing the event object

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,18 @@
 const Home = () => {
+    // where e is equal to the event object
+    const handleClick = (e) => {
+        console.log('Hello, Ninja', e);
+    }
+
+    const handleClickAgain = (name, e) => {
+        console.log('Hello, '+name);
+    }
+
     return ( 
         <div className="home">
-            <h2>Homepage</h2>  
+            <h2>Homepage</h2>
+            <button onClick={handleClick}>Click Me</button> <br />
+            <button onClick={(e) => handleClickAgain('Ovie', e)}>Click me again</button>
         </div>
     );
 }
