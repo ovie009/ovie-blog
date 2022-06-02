8cc3e2202360801ef34698cd5bf697648dad9a86
ovie009 <ovieiffieu2much@gmail.com>
2022-06-02 12:17:01 +0100

    useEffect dependencies

--- src/Home.js
+++ src/Home.js
@@ -11,20 +11,41 @@ const Home = () => {
 
     const handleDelete = (id) => {
         let newBlog = blogs.filter((blog) => blog.id !== id);
-
         setBlogs(newBlog);
     }
 
+    
     // use effect is used to fire a function after every render
+    // useEffect(() => {
+    //     console.log('use effect run at all render');
+    // })
+    
+    // a second variable can be passed into useEffect
+    // which is known as a dependency
+    // it can be used to decide if useEffecet should be triggered after a certain render 
+    
+    // run after first render only
+    // useEffect(() => {
+    //     console.log('use effect first render');
+    // }, []) //passsing an empty array
+    
+    const [name, setName] = useState('Mike');
+    
+    const handleName = () => {
+        setName('Josh');
+    }
+    // run at first render and if name state is changed
     useEffect(() => {
-        console.log('use effect ran');
-    })
+        console.log('name state was changed');
+    }, [name]) //passsing an empty array
 
     return ( 
         <div className="home">
-            {/* passing props */}
             <Bloglist blogs={blogs} title='All blogs' handleDelete = {handleDelete}/>
-            {/* <Bloglist blogs={blogs.filter((blog) => blog.author === 'mario')} title='Mario's/> */}
+
+            <p>Reviewed by {name}</p>
+
+            <button type="button" className="change-reviwer" onClick={handleName}>Change Reviewer</button>
         </div>
     );
 }
