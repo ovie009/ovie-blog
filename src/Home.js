b16a168128c95fa7c94a0da58ff3cfc933358fb7
ovie009 <ovieiffieu2much@gmail.com>
2022-06-02 10:05:07 +0100

    setup useEffect

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Bloglist from './Bloglist'
 
 const Home = () => {
@@ -14,6 +14,11 @@ const Home = () => {
 
         setBlogs(newBlog);
     }
+
+    // use effect is used to fire a function after every render
+    useEffect(() => {
+        console.log('use effect ran');
+    })
 
     return ( 
         <div className="home">
