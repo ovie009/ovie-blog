01843e895f240cb0739192afcd0c1e541522473b
ovie009 <ovieiffieu2much@gmail.com>
2022-06-02 21:59:57 +0100

    conditional loading message

--- src/Home.js
+++ src/Home.js
@@ -5,21 +5,25 @@ const Home = () => {
 
     const [blogs, setBlogs] = useState(null);
 
-    
+    const [isPending, setIsPending] = useState(true);
     // run at first render and if name state is changed
     useEffect(() => {
-        fetch('http://localhost:8000/blogs')
-        .then(response => {
-            return response.json();
-        })
-        .then(data => {
-            console.log(data);
-            setBlogs(data);
-        })
+        setTimeout(() => {
+            fetch('http://localhost:8000/blogs')
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                // console.log(data);
+                setBlogs(data);
+                setIsPending(false);
+            });
+        }, 1000);
     }, []) //passsing an empty array
 
     return ( 
         <div className="home">
+            {isPending && <div className="loading">loading...</div> }
             {blogs && <Bloglist blogs={blogs} title='All blogs'/>}
         </div>
     );
